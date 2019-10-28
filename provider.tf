beff7b52223963ed00b01548136a87c8a628d049
DevopsKristiania <tharinchobkaphol@gmail.com>
2019-10-29 00:51:51 +0100

    aws encryption keys

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "heroku" {
   version = "2.2"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "pgr301bucket"
+    key    = "pgr301exam/terraform.tfstate"
+    region = "eu-north-1"
+  }
+}
